b449493d2423ba3ad23564d0f50f047903d7c494
crispysocks <crispysocks@example.com>
2026-05-24 16:17:25 +0800

    feat: add ClassRepository

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,4 +1,5 @@
 from app.repositories.student_repository import StudentRepository
 from app.repositories.teacher_repository import TeacherRepository
+from app.repositories.class_repository import ClassRepository
 
-__all__ = ["StudentRepository", "TeacherRepository"]
+__all__ = ["StudentRepository", "TeacherRepository", "ClassRepository"]
